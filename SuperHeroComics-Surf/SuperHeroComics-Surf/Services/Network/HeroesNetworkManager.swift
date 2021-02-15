52367e0fa09544bc83d0f9e0683306c3728c7ec9
Air? <68377611+air-igor@users.noreply.github.com>
2021-02-16 01:59:34 +0300

    configure HeroesNetworkManager

---
+++ SuperHeroComics-Surf/SuperHeroComics-Surf/Services/Network/HeroesNetworkManager.swift
@@ -0,0 +1,76 @@
+//
+//  HeroesNetworkManager.swift
+//  SuperHeroComics-Surf
+//
+//  Created by Игорь Попов on 15.02.2021.
+//  Copyright © 2021 AirIgor. All rights reserved.
+//
+
+import Foundation
+
+final class HeroesNetworkManager {
+    
+    static let shared = HeroesNetworkManager()
+    typealias ErrorManager = (Errors) -> Void
+    
+    private let session = URLSession(configuration: .default)
+    private var dataTask: URLSessionDataTask?
+    
+    func fetchRandomHeroesList(onCompletion: @escaping ([HeroList]) -> Void,
+                               onError: @escaping ErrorManager) {
+        let mainUrl = UrlConfiguration.startUrl + UrlConfiguration.apiKey + UrlConfiguration.searchScheme + "\(randomHero(length: 1))"
+        
+        guard let url = URL(string: mainUrl) else {
+            return onError(.errorLink)
+        }
+        dataTask = session.dataTask(with: url, completionHandler: { (data, _, _) in
+            
+            guard let data = data else {
+                return onError(.errorResponse)
+            }
+            let decoder = JSONDecoder()
+            guard let objects = try? decoder.decode(HeroModel.self, from: data) else {
+                return onError(.errorParsing)
+            }
+            
+            DispatchQueue.main.async {
+                onCompletion(objects.results)
+            }
+        })
+        
+        dataTask?.resume()
+    }
+    
+    func fetchHero(searchText: String,
+                   onCompletion: @escaping ([HeroList]) -> Void,
+                   onError: @escaping ErrorManager) {
+        let mainUrl = UrlConfiguration.startUrl + "\(searchText.replacingOccurrences(of: " ", with: "%20"))"
+        
+        guard let url = URL(string: mainUrl) else {
+            return onError(.errorLink)
+        }
+        dataTask = session.dataTask(with: url, completionHandler: { (data, _, _) in
+            guard let data = data else {
+                return onError(.errorResponse)
+            }
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            guard let objects = try? decoder.decode(HeroModel.self, from: data) else {
+                return onError(.errorParsing)
+            }
+            DispatchQueue.main.async {
+                onCompletion(objects.results)
+            }
+        })
+        dataTask?.resume()
+    }
+    
+}
+
+
+private extension HeroesNetworkManager {
+    func randomHero(length: Int) -> String {
+        let letters = "qwertyuiopasdfghjklzxcvbnm"
+        return String((0..<length).map{ _ in (letters.randomElement()!) })
+    }
+}
