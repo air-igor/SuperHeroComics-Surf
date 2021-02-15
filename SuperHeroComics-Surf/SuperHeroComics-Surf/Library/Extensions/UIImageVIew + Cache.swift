6e1202fb50627ba18d2b78e06db0ca941893fc72
Air? <68377611+air-igor@users.noreply.github.com>
2021-02-16 01:58:14 +0300

    configure UIImageView + Cache

---
+++ SuperHeroComics-Surf/SuperHeroComics-Surf/Library/Extensions/UIImageVIew + Cache.swift
@@ -0,0 +1,38 @@
+//
+//  UIImageVIew + Cache.swift
+//  SuperHeroComics-Surf
+//
+//  Created by Игорь Попов on 15.02.2021.
+//  Copyright © 2021 AirIgor. All rights reserved.
+//
+
+import UIKit
+
+let imageCache = NSCache<AnyObject, AnyObject>()
+
+// MARK: - UIImageView extension
+
+extension UIImageView {
+    
+    func loadImage(urlSting: String) {
+        guard let url = URL(string: urlSting) else { return }
+        image = nil
+        
+        if let imageFromCache = imageCache.object(forKey: urlSting as AnyObject) {
+            image = imageFromCache as? UIImage
+            return
+        }
+        ImageNetworking.downloadImage(url: url) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let data):
+                guard let imageToCache = UIImage(data: data) else { return }
+                imageCache.setObject(imageToCache, forKey: urlSting as AnyObject)
+                self.image = UIImage(data: data)
+            case .failure(_):
+                self.image = UIImage()
+                
+            }
+        }
+    }
+}
