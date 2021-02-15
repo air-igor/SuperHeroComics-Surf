486da1f0a1edf004f6ebe3a940ad0b7c135edc00
Air? <68377611+air-igor@users.noreply.github.com>
2021-02-16 01:59:18 +0300

    Create UrlConfiguration.swift

---
+++ SuperHeroComics-Surf/SuperHeroComics-Surf/Resources/Constants/UrlConfiguration.swift
@@ -0,0 +1,15 @@
+//
+//  UrlConfiguration.swift
+//  SuperHeroComics-Surf
+//
+//  Created by Игорь Попов on 15.02.2021.
+//  Copyright © 2021 AirIgor. All rights reserved.
+//
+
+import Foundation
+
+enum UrlConfiguration {
+    static let startUrl = "https://www.superheroapi.com/api.php/"
+    static let apiKey = "102455288553537"
+    static let searchScheme = "/search/"
+}
