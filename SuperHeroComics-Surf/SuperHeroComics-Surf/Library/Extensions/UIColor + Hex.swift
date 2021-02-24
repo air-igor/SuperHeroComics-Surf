87570d5a3fe46c7ad848a8b09fc8d92b3ce5e214
Air? <68377611+air-igor@users.noreply.github.com>
2021-02-25 02:18:05 +0300

    update uicolor extension

--- SuperHeroComics-Surf/SuperHeroComics-Surf/Library/Extensions/UIColor + Hex.swift
+++ SuperHeroComics-Surf/SuperHeroComics-Surf/Library/Extensions/UIColor + Hex.swift
@@ -12,6 +12,10 @@ import UIKit
 
 extension UIColor {
     convenience init(rgb: UInt) {
-       self.init(red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0, green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0, blue: CGFloat(rgb & 0x0000FF) / 255.0, alpha: CGFloat(1.0))
+        self.init(red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0, green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0, blue: CGFloat(rgb & 0x0000FF) / 255.0, alpha: CGFloat(1.0))
+    }
+    
+    var heroCustomViewColor: UIColor {
+        return UIColor(red: 241/255, green: 241/255, blue: 241/255, alpha: 1.0)
     }
 }
