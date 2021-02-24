604778f299d8e21c7760bb61b1ebc89e0beaa785
Air? <68377611+air-igor@users.noreply.github.com>
2021-02-24 14:16:34 +0300

    update HomeSuperHeroRouterInput

--- SuperHeroComics-Surf/SuperHeroComics-Surf/User Stories/HomeSuperHero/Router/HomeSuperHeroInput.swift
+++ SuperHeroComics-Surf/SuperHeroComics-Surf/User Stories/HomeSuperHero/Router/HomeSuperHeroInput.swift
@@ -7,6 +7,6 @@
 //
 
 protocol HomeSuperHeroRouterInput {
-    func showDetailModule(heroes: [HeroList])
+    func showDetailModule(heroes: HeroList)
     func showMessageModule(with message: String)
 }
