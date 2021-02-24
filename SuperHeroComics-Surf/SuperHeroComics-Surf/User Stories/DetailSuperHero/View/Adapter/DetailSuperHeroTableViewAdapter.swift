82bd4d3b536f0becadea32eac5336e4ffd29043b
Air? <68377611+air-igor@users.noreply.github.com>
2021-02-24 14:15:30 +0300

    refactor code

--- SuperHeroComics-Surf/SuperHeroComics-Surf/User Stories/DetailSuperHero/View/Adapter/DetailSuperHeroTableViewAdapter.swift
+++ SuperHeroComics-Surf/SuperHeroComics-Surf/User Stories/DetailSuperHero/View/Adapter/DetailSuperHeroTableViewAdapter.swift
@@ -11,8 +11,8 @@ import UIKit
 final class DetailSuperHeroTableViewAdapter: NSObject {
     
     // MARK: - Private properties
-        
-    private var items: [DetailSuperHeroViewModel] = []
+    
+    private var cells: [UITableViewCell] = []
     private var tableView: UITableView
     
     // MARK: - Initialization
@@ -24,37 +24,52 @@ final class DetailSuperHeroTableViewAdapter: NSObject {
     }
     
     // MARK: - Internal methods
-
-    func configure(with items: [DetailSuperHeroViewModel]) {
-        self.items = items
+    
+    func configure(with items: DetailSuperHeroViewModel) {
+        configureCells(with: items)
         tableView.reloadData()
     }
-
 }
 
 // MARK: - UITableViewDataSource
 
 extension DetailSuperHeroTableViewAdapter: UITableViewDataSource {
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return cells.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueCell(withType: DetailSuperHeroTableViewCell.self, for: indexPath) as? DetailSuperHeroTableViewCell else {
-            return UITableViewCell()
-        }
-
-        cell.configure(with: items[indexPath.row])
-        
-        return cell
+        return cells[indexPath.row]
     }
-
+    
 }
 
 // MARK: - Private Methods
 
 private extension DetailSuperHeroTableViewAdapter {
+    
+    func configureCells(with model: DetailSuperHeroViewModel) {
+        if
+            let headerModel = model.header,
+            let cell = DetailSuperHeroTableViewCell.loadFromNib() as? DetailSuperHeroTableViewCell {
+            cell.configure(with: headerModel)
+            self.cells.append(cell)
+        }
+        
+        let cells = [
+            model.biographyParameters,
+            model.workParameters,
+            model.connectionsParamaters
+            ].compactMap {$0}.map { model -> UITableViewCell in
+                guard let cell = DetailSuperHeroParametersTableViewCell.loadFromNib() as? DetailSuperHeroParametersTableViewCell else {
+                    return UITableViewCell()
+                }
+                cell.configure(with: model.title)
+                return cell
+        }
+        self.cells.append(contentsOf: cells)
+    }
     
     func configureTableView() {
         tableView.registerCell(type: DetailSuperHeroTableViewCell.self)
@@ -64,5 +79,5 @@ private extension DetailSuperHeroTableViewAdapter {
         tableView.showsHorizontalScrollIndicator = false
         tableView.separatorStyle = .none
     }
-
+    
 }
