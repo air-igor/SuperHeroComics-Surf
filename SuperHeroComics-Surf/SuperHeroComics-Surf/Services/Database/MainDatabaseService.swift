b5ebaffbeb079bcc0a1ee2301e7cf87c2dbcfa6b
Air? <68377611+air-igor@users.noreply.github.com>
2021-02-26 21:52:21 +0300

    added MainDatabaseService

---
+++ SuperHeroComics-Surf/SuperHeroComics-Surf/Services/Database/MainDatabaseService.swift
@@ -0,0 +1,50 @@
+//
+//  MainDatabaseService.swift
+//  SuperHeroComics-Surf
+//
+//  Created by Игорь Попов on 26.02.2021.
+//  Copyright © 2021 AirIgor. All rights reserved.
+//
+
+import RealmSwift
+
+class MainDatabaseService {
+    
+    // MARK: - Properties
+    
+    static let shared = MainDatabaseService()
+    var realm = try! Realm()
+    
+    // MARK: - Internal Methods
+    
+    func update<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.add(object, update: .modified)
+            }
+        } catch {
+            print("Eror update \(error)")
+        }
+    }
+    
+    func save<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.add(object)
+            }
+        } catch {
+            print("Eror save \(error)")
+        }
+    }
+    
+    func delete<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.delete(object)
+            }
+        } catch {
+            print("Eror delete \(error)")
+        }
+    }
+    
+}
